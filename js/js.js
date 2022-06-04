3edabe4ccbd39d956513cabd00f8e4d0f3c95ad4
Markys97 <70927348+Markys97@users.noreply.github.com>
2022-06-05 01:42:41 +0300

    Add files via upload
    project end

--- js/js.js
+++ js/js.js
@@ -2,7 +2,6 @@ let burgerBtn= document.querySelector('.burger');
 let menuBlock= document.querySelector('.menu');
 let closeMenuBtn= document.querySelector('.menu__close-btn')
 let menuLinks= document.querySelectorAll('.menu__link')
-let textLinks=  document.querySelectorAll('.menu__link span')
 
 burgerBtn.addEventListener('click',e=>{
     menuBlock.classList.add('open-menu');
@@ -19,28 +18,11 @@ const setCloseMenu=()=>{
     }
 }
 
-menuLinks.forEach((link,index,links)=>{
+menuLinks.forEach(link=>{
     link.addEventListener('click',e=>{
         menuBlock.classList.remove('open-menu');
-        if(link.classList.contains('menu__link--active')){
-            return true
-        }else{
-            links.forEach(perLink=>{
-                perLink.classList.remove('menu__link--active')
-            })
-            link.classList.add('menu__link--active')
-        }
     })
 })
 
 window.addEventListener('resize',setCloseMenu)
 setCloseMenu()
-
-// change color menu cible logic
-
-// let url= window.location.href;
-// let locationSectionActive= url.slice(-9)
-
-
-
-
